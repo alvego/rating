3ba404f40408e55a90a6abc3987cc2e7ca6e8494
Aleksey Timofeev <Aleksey.Timofeev@yessoftware.com>
2014-04-29 11:56:04 +0400

    init

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-Rating
+jQuery 5-star rating plugin
 ======
 
 HTML5 CSS3 jQuery rating (5 stars) Only one span in CSS. Without images. Not need js in read-only mode.
+
+Using
+
+    <span id="rating1" data-rating="3"></span>
+
+    $('#rating1').rating(
+        {'change': function(selectedRating, callback){
+            // async code (ajax request)
+            //...
+            // set new value (not necessarily)
+            callback(selectedRating);
+        }}
+    );
+
---
+++ jquery.rating.js
@@ -0,0 +1,45 @@
+/*
+ * jQuery 5-star rating plugin
+ * Version 1.0
+ * https://github.com/alvego/rating
+ *
+ * jQuery 5-star rating plugin (necessary for user interaction).
+ *
+ * Copyright (c) 2014 http://rating.themext.com
+ * Licensed under the MIT and GPL licenses.
+ */
+
+(function( $ ){
+  $.fn.rating = function( options ) {  
+    var settings = $.extend( {
+      'change' : null
+    }, options);
+    return this.each(function() {    
+        var rating = 0;
+        var prop = 'data-rating' 
+        var control = $(this);
+        control.attr('data-enabled','').hover(
+            function(){
+                rating = control.attr(prop);
+                control.attr(prop, 0);
+            },
+            function(){ 
+                control.attr(prop,  rating); 
+            }
+        ).click(function(){
+            if (rating == control.attr(prop)) {
+                return;
+            }
+            rating = control.attr(prop);
+            if (null !== settings.change) {
+                settings.change(parseInt(rating), function(result){
+                    rating = Math.min(Math.max(parseInt(result), 0), 5);
+                    control.attr(prop,  rating);
+                });
+            } 
+        }).mousemove(function(e) { 
+            $(this).attr(prop, Math.floor(e.offsetX * 5 / $(this).width()+1));
+        });
+    });
+  };
+})( jQuery );
